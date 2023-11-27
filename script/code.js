d462f33dc624ce67e2b52f8e1c0057818a5df91e
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-11-27 10:25:11 +0200

    doItLater

--- script/code.js
+++ script/code.js
@@ -1,1 +1,9 @@
 //function
+let generate = document.getElementById('#btn')
+
+function generateRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    document.getElementById('#randomNumber').textContent = randomNumber
+}
+
+generate.addEventListener('click', generateRandomNumber)
